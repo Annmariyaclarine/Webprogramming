2adc716b5ffefc31c1f707eedcba188ce56716ea
Annmariyaclarine <95731201+Annmariyaclarine@users.noreply.github.com>
2022-03-03 12:04:18 +0530

    Add files via upload

---
+++ loginp.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+Ph.NO:<input type="text" name="ph"><br>
+<input type="submit">
+</form>
+</body>
+</html>
